77c2812f273c61e9943efa0fc021501fccb84e0f
saulelabra <quiquelabra@gmail.com>
2019-11-11 17:22:36 -0600

    scene setup

---
+++ light_bike/game.js
@@ -0,0 +1,78 @@
+var renderer = null;
+var scene = null;
+var camera = null;
+var root = null;
+var bikes_group = null;
+var group = null;
+var animator = null;
+
+var directionalLight = null;
+var spotLight = null;
+var ambientLight = null;
+var mapUrl = "../images/checker_large.gif";
+var SHADOW_MAP_WIDTH = 2048, SHADOW_MAP_HEIGHT = 2048;
+
+function createScene(canvas) {
+    // Create the Three.js renderer and attach it to our canvas
+    renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
+
+    // Set the viewport size
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Turn on shadows
+    renderer.shadowMap.enabled = true;
+    // Options are THREE.BasicShadowMap, THREE.PCFShadowMap, PCFSoftShadowMap
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    
+    // Create a new Three.js scene
+    scene = new THREE.Scene();
+
+    // Add  a camera so we can view the scene
+    camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 4000 );
+    camera.position.set(0, 15, 120);
+    scene.add(camera);
+        
+    // Create a group to hold all the objects
+    root = new THREE.Object3D;
+
+    ambientLight = new THREE.AmbientLight ( 0x888888 );
+    root.add(ambientLight);
+
+    // Create a group to hold the objects
+    group = new THREE.Object3D;
+    root.add(group);
+    //root.add(bike_group);
+
+    // Create a texture map
+    var map = new THREE.TextureLoader().load(mapUrl);
+    map.wrapS = map.wrapT = THREE.RepeatWrapping;
+    map.repeat.set(8, 8);
+
+    var color = 0xffffff;
+
+    // Put in a ground plane to show off the lighting
+    geometry = new THREE.PlaneGeometry(200, 200, 50, 50);
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({color:color, map:map, side:THREE.DoubleSide}));
+
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.position.y = -4.02;
+    
+    // Add the mesh to our group
+    group.add( mesh );
+    mesh.castShadow = false;
+    mesh.receiveShadow = true;
+    
+    // Now add the group to our scene
+    scene.add( root );
+}
+
+function run() {     
+    requestAnimationFrame(function() { run(); });
+
+    // Render the scene
+    renderer.render( scene, camera );
+    KF.update();
+}
+
+
+
