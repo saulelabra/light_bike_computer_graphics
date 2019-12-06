27974eff619030cc8221a2a7b82bbd4fa63eeb4e
RodrigoGM97 <rodrigogm971@gmail.com>
2019-12-05 18:14:54 -0600

    panel

---
+++ light_bike/clases/panel.js
@@ -0,0 +1,15 @@
+class Panel {
+    constructor() {
+        //var lineMaterial = new THREE.LineBasicMaterial({color: 0xff00ff, linewidth:2});
+    }
+
+    createWall(bikePosition) {
+        var newPlane = plane.clone();
+
+        newPlane.position.x = bikePosition.x;
+        newPlane.position.y = bikePosition.y;
+        newPlane.position.z = bikePosition.z;
+        scene.add( newPlane );
+        console.log("coords %j", newPlane.rotation);
+    }
+}
