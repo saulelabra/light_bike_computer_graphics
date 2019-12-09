c3c1b8484ce745a97e2a854d2b3ddfd89df43425
RodrigoGM97 <rodrigogm971@gmail.com>
2019-12-09 16:02:30 -0600

    a

--- light_bike/loadModels.js
+++ light_bike/loadModels.js
@@ -24,7 +24,6 @@ async function loadGreenBikeMTL() {
             tron_bike_green.position.x = -10;
             tron_bike_green.name = "tron_bike_green";
             
-            tron_bike_green_new = tron_bike_green;
             //scene.add( tron_bike_green );
         });
     });    
@@ -54,8 +53,6 @@ async function loadBlueBikeMTL() {
             camera1.position.z += 10;
             tron_bike_blue.position.x = 10;
             tron_bike_blue.name = "tron_bike_blue";
-
-            tron_bike_blue_new = tron_bike_blue;
             
             //scene.add( tron_bike_blue );
         });
--- light_bike/setStart.js
+++ light_bike/setStart.js
@@ -1,7 +1,4 @@
 function setStart() {
-
-    //tron_bike_blue = tron_bike_blue_new;
-    console.log(tron_bike_blue_new);
     scene.add(tron_bike_green);
     scene.add(tron_bike_blue);
     roundOver = false;
