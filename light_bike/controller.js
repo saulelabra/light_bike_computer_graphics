90ac43d9c880452f165517aa619f2d97becb6349
RodrigoGM97 <rodrigogm971@gmail.com>
2019-12-09 16:21:53 -0600

    asdf

--- light_bike/controller.js
+++ light_bike/controller.js
@@ -3,8 +3,6 @@ var controller = async function() {
     
     if(roundOver == false) {
         document.addEventListener("keydown", onDocumentKeyDown, false);
-        var x = new Panel();
-        x.createWall();
     }
     
     
